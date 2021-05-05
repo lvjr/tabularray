31cb8871e779e7cfb71abb601e7acf1347e822a1
lvjr <tolvjr@163.com>
2021-05-05 19:41:37 +0800

    add build.lua

---
+++ build.lua
@@ -0,0 +1,14 @@
+
+module = "tabularray"
+
+sourcefiles = {"tabularray.sty"}
+
+checkengines = {"pdftex","xetex"}
+checkruns = 1
+
+lvtext = ".tex"
+
+kpse.set_program_name ("kpsewhich")
+if not release_date then
+ dofile ( kpse.lookup ("l3build.lua"))
+end
