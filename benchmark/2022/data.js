140036f3cae692fc271ac1665f9860f5119e380d
github-action-benchmark <github@users.noreply.github.com>
2025-10-29 06:51:29 +0000

    add Tabularray Benchmark in TeX Live 2022 (customSmallerIsBetter) benchmark result for a9be79e042e28ee8488e3b72dcf9408a5787942c

--- benchmark/2022/data.js
+++ benchmark/2022/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1757251373354,
+  "lastUpdate": 1761720689885,
   "repoUrl": "https://github.com/lvjr/tabularray",
   "entries": {
     "Tabularray Benchmark in TeX Live 2022": [
@@ -6552,6 +6552,48 @@ window.BENCHMARK_DATA = {
             "value": 0.37770252103498,
             "unit": "ratio",
             "extra": "current time : previous time = 2.1864051818848 : 5.7886962890625"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tolvjr@163.com",
+            "name": "lvjr",
+            "username": "lvjr"
+          },
+          "committer": {
+            "email": "tolvjr@163.com",
+            "name": "lvjr",
+            "username": "lvjr"
+          },
+          "distinct": true,
+          "id": "a9be79e042e28ee8488e3b72dcf9408a5787942c",
+          "message": "improve page tracing log",
+          "timestamp": "2025-10-29T14:47:47+08:00",
+          "tree_id": "3c2ae225b029f99c3ef84e84bf40e702fc60294a",
+          "url": "https://github.com/lvjr/tabularray/commit/a9be79e042e28ee8488e3b72dcf9408a5787942c"
+        },
+        "date": 1761720689094,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "compile tabularray with pdflatex",
+            "value": 0.34486193025794,
+            "unit": "ratio",
+            "extra": "current time : previous time = 1.669563293457 : 4.841251373291"
+          },
+          {
+            "name": "compile tabularray with xelatex",
+            "value": 0.35037823486143,
+            "unit": "ratio",
+            "extra": "current time : previous time = 1.675350189209 : 4.7815475463867"
+          },
+          {
+            "name": "compile tabularray with lualatex",
+            "value": 0.37956426837995,
+            "unit": "ratio",
+            "extra": "current time : previous time = 2.1711616516113 : 5.720142364502"
           }
         ]
       }
